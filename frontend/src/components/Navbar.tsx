7303b6bc80d59fc27e5a306a2a1840f51797842f
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2025-07-03 16:40:13 +0100

    added glassmorphism to the NavBar and booking page content into components

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -14,14 +14,15 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <>
-      <nav className="sticky top-0 left-0 z-10 bg-[#FFFFFF] shadow-[0_2px_4px_rgba(0,0,0,0.1)] px-12 py-[10px] flex justify-between items-center">
-        <h1 className="text-lg font-bold">MAR ABU HOMES</h1>
-        <div className="flex flex-column gap-4">
+      <nav className="sticky top-0 left-0 z-10 bg-white/10 backdrop-blur-md border-b border-white/20 shadow-[0_4px_12px_rgba(0,0,0,0.05)] px-12 py-[10px] flex justify-between items-center">
+        <h1 className="text-lg font-bold text-gray-900">MAR ABU HOMES</h1>
+        <div className="flex flex-row gap-4">
           {navItem.map(({ label, href }) => (
             <Link
               key={label}
               href={href}
               className={classNames(
+                "text-gray-800 hover:text-primary transition-colors duration-200",
                 pathname === href && "text-primary font-semibold"
               )}
             >
