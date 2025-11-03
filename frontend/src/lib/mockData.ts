ce86d480bf7138744b3780cfa276fc2571ceea52
remilekun-hub <atandaremilekun@gmail.com>
2025-11-03 14:10:44 +0100

    obudu 8 id refix

--- frontend/src/lib/mockData.ts
+++ frontend/src/lib/mockData.ts
@@ -104,7 +104,7 @@ export const properties: Property[] = [
 	},
 
 	{
-		id: "cmhi80phg0007fk4u73pxj2g",
+		id: "cmhi80phg0007fk4u73pxj2gt",
 		name: "OBUDU VILLA 8",
 		type: "Short Let",
 		location: "lakowe lakes golf & country estate, ibeju-lekki",
